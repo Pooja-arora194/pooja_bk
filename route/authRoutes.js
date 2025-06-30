d3b916ed23fba2364b8bb80288f88253efdb0aec
pooja194 <pooja987743@gmail.com>
2025-06-30 09:30:25 +0530

    second

--- route/authRoutes.js
+++ route/authRoutes.js
@@ -5,20 +5,10 @@ import { authenticate } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.post('/register', [
-    body('fullName').notEmpty().withMessage('Full name is required'),
-    body('email').isEmail().withMessage('Valid email is required'),
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
-    body('confirmPassword').notEmpty().withMessage('Confirm password is required'),
-], registerUser);
+router.post('/register',  registerUser);
 
-router.post('/login', [
-    body('email').isEmail().withMessage('Valid email is required'),
-    body('password').notEmpty().withMessage('Password is required'),
-], loginUser);
+router.post('/login',  loginUser);
 
-router.get('/dashboard', authenticate, (req, res) => {
-    res.json({ message: `Welcome ${req.user.email}, you're authorized.` });
-});
+
 
 export default  router;
